875a34ea5ef50cfb1ceb0c18b1f8c8796efa9d96
peacekeeper <markus.sabadello@gmail.com>
2013-04-26 01:13:45 +0200

    WIP for updated XDI discovery

--- src/main/java/xdi2/xrinet/XdiProxyMessagingTarget.java
+++ src/main/java/xdi2/xrinet/XdiProxyMessagingTarget.java
@@ -1,7 +1,12 @@
 package xdi2.xrinet;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.openxri.XRI;
 import org.openxri.proxy.impl.AbstractProxy;
@@ -9,20 +14,23 @@ import org.openxri.resolve.Resolver;
 import org.openxri.resolve.ResolverFlags;
 import org.openxri.resolve.ResolverState;
 import org.openxri.resolve.exception.PartialResolutionException;
-import org.openxri.util.PrioritizedList;
+import org.openxri.xml.SEPType;
+import org.openxri.xml.SEPUri;
 import org.openxri.xml.Service;
 import org.openxri.xml.Status;
 import org.openxri.xml.XRD;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import xdi2.core.ContextNode;
 import xdi2.core.Graph;
 import xdi2.core.constants.XDIConstants;
-import xdi2.core.features.dictionary.Dictionary;
 import xdi2.core.features.equivalence.Equivalence;
-import xdi2.core.features.multiplicity.XdiAttributeSingleton;
-import xdi2.core.features.multiplicity.XdiSubGraph;
-import xdi2.core.features.roots.PeerRoot;
-import xdi2.core.features.roots.Roots;
+import xdi2.core.features.nodetypes.XdiAttributeClass;
+import xdi2.core.features.nodetypes.XdiAttributeInstance;
+import xdi2.core.features.roots.XdiLocalRoot;
+import xdi2.core.features.roots.XdiPeerRoot;
+import xdi2.core.util.XDI3Util;
 import xdi2.core.xri3.XDI3Segment;
 import xdi2.core.xri3.XDI3SubSegment;
 import xdi2.messaging.GetOperation;
@@ -35,8 +43,10 @@ import xdi2.messaging.target.ExecutionContext;
 
 public class XdiProxyMessagingTarget extends AbstractMessagingTarget {
 
-	public static final String XRI_URI = "$uri";
-	public static final String STRING_TYPE_XDI = "$xdi$*($v)$!1";
+	private static final Logger log = LoggerFactory.getLogger(XdiProxyMessagingTarget.class);
+
+	public static final XDI3SubSegment XRI_URI = XDI3SubSegment.create("$uri");
+	public static final String STRING_TYPE_XDI = "$xdi";
 	public static final XDI3Segment XRI_TYPE_XDI = XDI3Segment.create(STRING_TYPE_XDI);
 
 	private AbstractProxy proxy;
@@ -68,13 +78,13 @@ public class XdiProxyMessagingTarget extends AbstractMessagingTarget {
 		@Override
 		public void getContext(XDI3Segment targetAddress, GetOperation operation, MessageResult messageResult, ExecutionContext executionContext) throws Xdi2MessagingException {
 
-			// is this a peer root context XRI?
+			// prepare XRI
 
 			XDI3Segment xri;
 
-			if (PeerRoot.isPeerRootXri(targetAddress.getLastSubSegment())) {
+			if (XdiPeerRoot.isPeerRootArcXri(targetAddress.getLastSubSegment())) {
 
-				xri = PeerRoot.getXriOfPeerRootXri(targetAddress.getLastSubSegment());
+				xri = XdiPeerRoot.getXriOfPeerRootArcXri(targetAddress.getLastSubSegment());
 			} else {
 
 				xri = targetAddress;
@@ -85,15 +95,13 @@ public class XdiProxyMessagingTarget extends AbstractMessagingTarget {
 			Resolver resolver = XdiProxyMessagingTarget.this.proxy.getResolver();
 
 			ResolverFlags resolverFlags = new ResolverFlags();
-			resolverFlags.setNoDefaultT(true);
-
 			ResolverState resolverState = new ResolverState();
 
 			XRD xrd;
 
 			try {
 
-				xrd = resolver.resolveSEPToXRD(new XRI(xri.toString()), "xri://$xdi!($v!1)", null, resolverFlags, resolverState);
+				xrd = resolver.resolveSEPToXRD(new XRI(xri.toString()), null, null, resolverFlags, resolverState);
 			} catch (PartialResolutionException ex) {
 
 				xrd = ex.getPartialXRDS().getFinalXRD();
@@ -104,23 +112,36 @@ public class XdiProxyMessagingTarget extends AbstractMessagingTarget {
 				throw new Xdi2MessagingException(xrd.getStatus().getValue(), null, executionContext);
 			}
 
-			// extract inumber and URI
+			// extract cloudnumber
 
-			XDI3Segment inumber = XDI3Segment.create(xrd.getCanonicalID().getValue());
-			List<String> uris = new ArrayList<String> ();
+			XDI3Segment cloudnumber = XDI3Util.canonicalIdToCloudnumber(xrd.getCanonicalID().getValue());
 
-			PrioritizedList selectedServicesPrioritizedList = xrd.getSelectedServices();
-			ArrayList<?> selectedServices = selectedServicesPrioritizedList == null ? null : selectedServicesPrioritizedList.getList();
+			// extract URIS
 
-			if (selectedServices != null) {
+			Map<String, List<String>> uriMap = new HashMap<String, List<String>> ();
 
-				for (int i=0; i<selectedServices.size(); i++) {
+			for (int i=0; i<xrd.getNumServices(); i++) {
 
-					Service selectedService = (Service) selectedServices.get(i);
+				Service service = xrd.getServiceAt(i);
+				if (service.getNumTypes() == 0) continue;
 
-					for (int ii=0; ii<selectedService.getNumURIs(); ii++) {
+				for (int ii=0; ii<service.getNumTypes(); ii++) {
 
-						uris.add(selectedService.getURIAt(ii).getUriString());
+					SEPType type = service.getTypeAt(ii);
+
+					List<String> uriList = uriMap.get(type.getType());
+
+					if (uriList == null) {
+
+						uriList = new ArrayList<String> ();
+						uriMap.put(type.getType(), uriList);
+					}
+
+					for (int iii = 0; iii<service.getNumURIs(); iii++) {
+
+						SEPUri uri = service.getURIAt(iii);
+
+						uriList.add(uri.getUriString());
 					}
 				}
 			}
@@ -129,32 +150,62 @@ public class XdiProxyMessagingTarget extends AbstractMessagingTarget {
 
 			Graph graph = messageResult.getGraph();
 
-			// add "self" peer root context nodes
+			// add "self" peer root
 
-			Roots.findLocalRoot(graph).setSelfPeerRoot(XDIConstants.XRI_S_ROOT);
+			XdiLocalRoot.findLocalRoot(graph).setSelfPeerRoot(XDIConstants.XRI_S_ROOT);
 
-			// add I-Number peer root context nodes
+			// add cloudnumber peer root
 
-			ContextNode inumberPeerRootContextNode = Roots.findLocalRoot(graph).findPeerRoot(inumber, true).getContextNode();
+			XdiLocalRoot.findLocalRoot(graph).findPeerRoot(cloudnumber, true);
 
 			// add URIs
 
-			if (uris.size() > 0) {
+			XdiAttributeClass uriXdiAttributeClass = XdiLocalRoot.findLocalRoot(graph).getXdiAttributeClass(XRI_URI, true);
 
-				XdiAttributeSingleton uriAttributeSingleton = XdiSubGraph.fromContextNode(inumberPeerRootContextNode).getAttributeSingleton(XDI3SubSegment.create(XRI_URI), true);
-				Dictionary.addContextNodeType(uriAttributeSingleton.getContextNode(), XRI_TYPE_XDI);
-				uriAttributeSingleton.getContextNode().createLiteral(uris.get(0));
+			for (Entry<String, List<String>> uriMapEntry : uriMap.entrySet()) {
+
+				String type = uriMapEntry.getKey();
+				List<String> uriList = uriMapEntry.getValue();
+
+				for (String uri : uriList) {
+
+					XdiAttributeInstance uriXdiAttributeInstance = uriXdiAttributeClass.setXdiInstanceUnordered(null);
+					uriXdiAttributeInstance.getXdiValue(true).getContextNode().setLiteral(uri);
+
+					XdiAttributeClass typeXdiAttributeClass = XdiLocalRoot.findLocalRoot(graph).getXdiEntitySingleton(typeToSubSegment(type), true).getXdiAttributeClass(XRI_URI, true);
+					XdiAttributeInstance typeXdiAttributeInstance = typeXdiAttributeClass.setXdiInstanceOrdered(-1);
+					Equivalence.setReferenceContextNode(typeXdiAttributeInstance.getContextNode(), uriXdiAttributeInstance.getContextNode());
+				}
 			}
 
-			// add I-Number and original XRI
+			// add cloudnumber and original XRI
 
-			ContextNode inumberContextNode = graph.findContextNode(inumber, true);
+			ContextNode cloudnumberContextNode = graph.setDeepContextNode(cloudnumber);
 
-			if (! xri.equals(inumber)) {
+			if (! xri.equals(cloudnumber)) {
 
-				ContextNode xriContextNode = graph.findContextNode(xri, true);
-				Equivalence.setReferenceContextNode(xriContextNode, inumberContextNode);
+				ContextNode xriContextNode = graph.setDeepContextNode(xri);
+				Equivalence.setReferenceContextNode(xriContextNode, cloudnumberContextNode);
 			}
 		}
 	};
+
+	private static XDI3SubSegment typeToSubSegment(String type) {
+
+		log.info(type);
+		
+		try {
+
+			return XDI3SubSegment.create(type);
+		} catch (Exception ex) {
+
+			try {
+
+				return XDI3SubSegment.create("(" + URLEncoder.encode(type, "UTF-8") + ")");
+			} catch (UnsupportedEncodingException ex2) {
+
+				return null;
+			}
+		}
+	}
 }
